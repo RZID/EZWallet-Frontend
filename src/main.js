c7540620a983ac834712804715af6ea37a2c49c1
RZIDInc <rdhanukpb@gmail.com>
2021-02-23 14:39:37 +0700

    Push login

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/fontawesome/fontawesome-free-5.15.1-web/css/all.min.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
